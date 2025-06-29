6d92320359fe9162f644b676fb53b8d7b572daa5
Josh 'Jekotia' Ameli <contact@jekotia.net>
2025-06-29 12:35:36 -0400

    Remove unmaintained images from Raspberry Pi days.

--- weechat/copy/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-usermod -o -u "$GID" $USER
-groupmod -o -g "$UID" $GROUP
-
-chown -R $USER:$GROUP $DATA
-chown -R $USER:$GROUP $HOME
-
-exec su-exec $USER /usr/bin/weechat-headless -d $DATA
